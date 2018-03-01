1fafa70e8f5c71e9f966e5898e0e8bdbf4dd0762
chenyufeng <chenyufeng@DESKTOP-0D1OC2O>
2018-03-01 16:10:48 +0800

    1

--- TemplateWeb/Controllers/HomeController.cs
+++ TemplateWeb/Controllers/HomeController.cs
@@ -3,14 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TemplateWeb.Models.DB;
 
 namespace TemplateWeb.Controllers
 {
     public class HomeController : Controller
     {
+        EntityDB entity = new EntityDB();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult IndexParam_Get()
+        {
+            var nav = entity.lay_nav_nav.Where(p => p.enable == true).OrderBy(p => p.sort).ToArray().Select(p => new
+            {
+                p.id,
+                p.title,
+                p.mode,
+                p.page_id,
+                p.url,
+                subnav = entity.lay_nav_subnav.Where(q => q.nav_id == p.id && q.enable == true).OrderBy(q => q.sort).Select(q => new
+                {
+                    q.id,
+                    q.title,
+                    q.mode,
+                    q.page_id,
+                    q.url,
+                }),
+            });
+            var notice = entity.lay_notice.Where(p => p.enable == true).OrderBy(p => p.sort).Select(p => new
+            {
+                p.id,
+                p.content,
+            });
+            var banner = entity.lay_banner.Where(p => p.enable == true).OrderBy(p => p.sort).Select(p => new
+            {
+                p.id,
+                p.title,
+                p.path,
+                p.mode,
+                p.page_id,
+                p.url,
+            });
+            var partner = entity.lay_partner.Where(p => p.enable == true).OrderBy(p => p.sort).Select(p => new
+            {
+                p.id,
+                p.title,
+                p.path,
+                p.url,
+            });
+            var link = entity.lay_link_link.Where(p => p.enable == true).OrderBy(p => p.sort).ToArray().Select(p => new
+            {
+                p.id,
+                p.title,
+                p.mode,
+                p.page_id,
+                p.url,
+                subnav = entity.lay_link_sublink.Where(q => q.link_id == p.id && q.enable == true).OrderBy(q => q.sort).Select(q => new
+                {
+                    q.id,
+                    q.title,
+                    q.mode,
+                    q.page_id,
+                    q.url,
+                }),
+            });
+            return Json(new
+            {
+                nav,
+                notice,
+                banner,
+                partner,
+                link,
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- TemplateWeb/Scripts/home/home-index.js
+++ TemplateWeb/Scripts/home/home-index.js
@@ -1,1 +1,14 @@
-﻿
+﻿var app = angular.module('app', []);
+app.controller('index', function ($scope, $http) {
+    $scope.Init = function () {
+        $scope.LoadData();
+    };
+    $scope.LoadData = function () {
+        $http.post('/Home/IndexParam_Get').success(function (d) {
+            $scope.param = d;
+        }).error(function () {
+            console.log('http错误');
+        });
+    };
+    $scope.Init();
+});
