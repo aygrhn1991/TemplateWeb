ca5c2e30a32e9f0ac19e9be5d137970f07644262
chenyufeng <chenyufeng@DESKTOP-0D1OC2O>
2018-03-14 16:20:29 +0800

    1

---
+++ TemplateWeb/Extension/AdminAuthorizeAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemplateWeb.Extension
+{
+    public class AdminAuthorizeAttribute: AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var cookie = httpContext.Request.Cookies["tpadmin"];
+            if (cookie == null)
+            {
+                return false;
+            }
+            else
+            {
+                string cookie_str = SecurityDES.Decrypt(cookie.Value);
+                try
+                {
+                    AdminCookieModel model = (AdminCookieModel)JsonConvert.DeserializeObject(cookie_str, typeof(AdminCookieModel));
+                    if (model.isauth == true)
+                    {
+                        if (Roles.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Length == 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            foreach (var item in Roles.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                            {
+                                if (model.roles.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Contains(item))
+                                {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Cookies["jjsadmin"] == null)
+            {
+                filterContext.Result = new RedirectResult("/Admin/Login");
+            }
+            else
+            {
+                filterContext.Result = new RedirectResult("/Home/Error?error=" + "您所在的用户组权限不足！");
+            }
+        }
+    }
+}
---
+++ TemplateWeb/Extension/SysTool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace TemplateWeb.Extension
+{
+    public class SysTool
+    {
+        public static string MD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] byteValue = Encoding.Unicode.GetBytes(str);
+            byte[] hashValue = md5.ComputeHash(byteValue);
+            string result = null;
+            for (int i = 0; i < hashValue.Length; i++)
+            {
+                result += hashValue[i].ToString("X");
+            }
+            return result;
+        }
+    }
+}
