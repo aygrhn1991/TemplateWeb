5e9156b6fc8679440724eec4c63a1049b62697ac
chenyufeng <chenyufeng@DESKTOP-0D1OC2O>
2018-03-19 16:17:29 +0800

    1

---
+++ TemplateWeb/Controllers/MemberController.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplateWeb.Extension;
+using TemplateWeb.Models.Account;
+using TemplateWeb.Models.DB;
+
+namespace TemplateWeb.Controllers
+{
+    public class MemberController : Controller
+    {
+        EntityDB entity = new EntityDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region 登陆
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(string phone, string password)
+        {
+            account_member member = entity.account_member.FirstOrDefault(p => p.phone == phone);
+            if (member != null && DESTool.Encrypt(password) == member.password)
+            {
+                if (member.enable == true)
+                {
+                    MemberModel model = new MemberModel()
+                    {
+                        phone = member.phone,
+                        password = member.password,
+                        isAuth = true,
+                    };
+                    HttpCookie cookie = new HttpCookie("tpadmin");
+                    string cookieStr = JsonConvert.SerializeObject(model);
+                    cookie.Value = DESTool.Encrypt(cookieStr);
+                    HttpContext.Response.AppendCookie(cookie);
+                    return Json(true, JsonRequestBehavior.AllowGet); ;
+                }
+                else
+                {
+                    return Json("账号已被停用", JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json("账号或密码错误", JsonRequestBehavior.AllowGet);
+        }
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            HttpCookie cookie = new HttpCookie("tpadmin");
+            cookie.Expires = DateTime.Now.AddDays(-1);
+            HttpContext.Response.AppendCookie(cookie);
+            return RedirectToAction("Login", "Admin");
+        }
+        #endregion
+    }
+}
