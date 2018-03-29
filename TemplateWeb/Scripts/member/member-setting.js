860142d175d0bc3bc7afde8ce98a9d4605a49531
chenyufeng <chenyufeng@DESKTOP-0D1OC2O>
2018-03-29 15:56:29 +0800

    1

--- TemplateWeb/Scripts/member/member-setting.js
+++ TemplateWeb/Scripts/member/member-setting.js
@@ -13,12 +13,43 @@ app.controller('info', function ($scope, $http, NgTableParams) {
             window.LayerClose();
         });
     };
-    $scope.Save = function (e) {
+    $scope.Save = function () {
         window.LayerOpen();
-        $http.post('/Member/Info_Add_Edit', e).success(function (d) {
+        $http.post('/Member/Info_Add_Edit', $scope.infoModel).success(function (d) {
             if (d == true) {
                 alert('保存成功');
                 $scope.LoadData();
+            } else {
+                alert('保存失败');
+                window.LayerClose();
+            }
+        }).error(function () {
+            console.log('http错误');
+            window.LayerClose();
+        });
+    };
+    $scope.Init();
+});
+app.controller('password', function ($scope, $http, NgTableParams) {
+    $scope.Init = function () {
+        $scope.passwordModel = {
+            password: null,
+            confirmPassword: null,
+        };
+    };
+    $scope.Save = function () {
+        if ($scope.passwordModel.confirmPassword != $scope.passwordModel.password) {
+            alert('两次密码不一致');
+            return;
+        }
+        window.LayerOpen();
+        $http.post('/Member/Password_Add_Edit', {
+            password: $scope.passwordModel.password,
+        }).success(function (d) {
+            if (d == true) {
+                alert('保存成功');
+                $scope.Init();
+                window.LayerClose();
             } else {
                 alert('保存失败');
                 window.LayerClose();
